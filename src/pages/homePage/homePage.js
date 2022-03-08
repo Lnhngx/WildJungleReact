30ee3d27b3765f674dc3b2fc5ec27c95c8cfea98
Yi-Ning <ning19950212@gmail.com>
2022-03-08 16:48:09 +0800

    homepage css

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,6 +1,31 @@
 import React from "react";
-
-function homePage(){
-    return(<></>)
+import "./homePage.css";
+function homePage() {
+  return (
+    <>
+      <div className="ninghomeCarouselWrap">
+        <ul className="ninghomeCarouselWrapImg">
+          <li>
+            <img src="/img/home/activity03.jpg" />
+          </li>
+          <li>
+            <img src="/img/home/activity01.jpeg" />
+          </li>
+          <li>
+            <img src="/img/home/activity02.jpg" />
+          </li>
+        </ul>
+      </div>
+      <div><img src="/img/home/homebgc(black).svg"></img></div>
+      <div className="ninghometicketbox">
+      <div className="ninghomeAdmissionbox"></div>
+        <div className="ninghomeAdmissionboxIcon">
+          <i className="homeIconfas fas fa-users"></i>
+          <p className="ninghomeAdmissionText">入園人數</p>
+        </div>
+        <div className="ninghomeAdmissionNumber">180</div>
+      </div>
+    </>
+  );
 }
-export default homePage
+export default homePage;
