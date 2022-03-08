6a367b49273e2b3b4a330dea5ecf7afbde3ece6c
tony-hsueh <yahooleo36@gmail.com>
2022-03-08 17:20:21 +0800

    遊戲圖片

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,8 +1,22 @@
 import React from "react";
+import './Game.css';
  function Game(){
      return(
      <>
-
+        <div className="container">
+            <div className="title">眼力大考驗</div>
+            <div className="leaf_left">
+                <img src="/leaf_left.png" alt=""/>
+            </div>
+            <div className="leaf_right">
+                <img src="/leaf_right.png" alt=""/>
+             
+                
+            </div>
+            <div className="cloud">
+                <svg src="./imgs/cloud.svg"></svg>
+            </div>
+        </div>
      </>
      )
  }
