4a582fcc36c5ba1c60e11b28e0cd19d5f8439b60
Stan Lin <love4210kimo@gmail.com>
2022-03-08 19:00:19 +0800

    Update carts js & css

--- src/pages/carts/carts.js
+++ src/pages/carts/carts.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./carts.css";
 
-function orders(){
-    return (<></>)
+function carts() {
+  return (
+    <>
+      <div className="stan-carts-main">
+        <div className="stan-process">
+          <div className="stan-process-iconarea">
+            <div className="stan-process-circle">1</div>
+            <div className="stan-process-line"></div>
+            <div className="stan-process-circle">2</div>
+            <div className="stan-process-line"></div>
+            <div className="stan-process-circle">3</div>
+          </div>
+        </div>
+        <div className="stan-process-word"></div>
+      </div>
+    </>
+  );
 }
 
-export default orders
+export default carts;
