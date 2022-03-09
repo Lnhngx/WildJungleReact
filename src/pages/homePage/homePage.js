586ad6b236c9300218b97d088520335bac040d09
Yi-Ning <ning19950212@gmail.com>
2022-03-09 23:53:43 +0800

    home css

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -17,32 +17,64 @@ function homePage() {
             </li>
           </ul>
         </div>
-        <div className="ning_ticketbox">
+        <div className="ning_bgcblack">
+            <img src="/img/home/homebgcblack.svg" alt="" />
+          </div>
+        <div className="ning_informationbox">
           <div className="ning_Admissionbox">
             <div className="ning_AdmissionboxIcon">
               <span className="material-icons">groups</span>
-              <p className="ninghometicket">入園人數</p>
-              <p className="ninghometicketEn">Ticket</p>
+              <p className="ninghomeAdmission">入園人數</p>
+              <p className="ninghomeAdmissionEn">Ticket</p>
             </div>
             <div className="ninghomeAdmissionCout">
               <div className="ninghomeAdmissionNumber">180</div>
               <p className="ninghomeAdmissionUnit">人</p>
             </div>
+            <div className="ning_ticketborderbox">
+              <div className="ning_ticketborder"></div>
+            </div>
+          </div>
+          <div className="ning_weatherbox">
+            <div className="ning_weatherboxIcon">
+              <i class="ning_weatherIcon fas fa-cloud"></i>
+              <p className="ninghomeweather">現在天氣</p>
+              <p className="ninghomeweatherEn">Wether</p>
+            </div>
+            <div className="ninghomeweatherCout">
+              <div className="ninghomeweatherNumber">18</div>
+              <p className="ninghomeweatherUnit">°C</p>
+            </div>
+            <div className="ning_ticketborderbox">
+              <div className="ning_ticketborder"></div>
+            </div>
+          </div>
+          <div className="ning_ticketbox">
+            <div className="ning_ticketboxIcon">
+              <span class="ning_ticketIcon material-icons">
+                confirmation_number
+              </span>
+              <p className="ninghometicket">票券資訊</p>
+              <p className="ninghometicketEn">Ticket</p>
+            </div>
+            <div className="ninghomeweatherCout">
+              <div className="ninghomeweatherNumber">18</div>
+              <p className="ninghomeweatherUnit">°C</p>
+            </div>
           </div>
         </div>
-        {/* <div className="ning_JungleTour">
-        <div className="ning_bgcblack"><img src="/img/home/homebgcblack.svg" alt="" /></div>
-        <div className="ning_JungleTourTitle">
-          <h1 className="ning_EnTitle">Jungle Tour</h1>
-          <p className="ning_ChTitle">園區分類一覽</p>
-          <div className="ning_JungleTourmap">
-            <img src="/img/home/tropicalmap.svg" alt="" />
+        <div className="ning_JungleTour">
+          <div className="ning_JungleTourTitle">
+            <h1 className="ning_EnTitle">Jungle Tour</h1>
+            <p className="ning_ChTitle">園區分類一覽</p>
+            <div className="ning_JungleTourmap">
+              <img src="/img/home/tropicalmap.svg" alt="" />
+            </div>
+          </div>
+          <div className="ning_tropical">
+            <img src="/img/home/tropical.jpg" alt="" />
           </div>
         </div>
-        <div className="ning_tropical">
-          <img src="/img/home/tropical.jpg" alt="" />
-        </div>
-      </div> */}
       </div>
     </>
   );
