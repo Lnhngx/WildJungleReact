0d131a6b7d206e9abfb187ff2901b76b1760a5f0
Yi-Ning <ning19950212@gmail.com>
2022-03-09 21:42:31 +0800

    index.html  link  googleicon

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -3,28 +3,34 @@ import "./ning.scss";
 function homePage() {
   return (
     <>
-      <div className="ning_CarouselWrap">
-        <ul className="ning_CarouselWrapImg">
-          <li>
-            <img src="/img/home/activity03.jpg" alt="" />
-          </li>
-          <li>
-            <img src="/img/home/activity01.jpeg" alt="" />
-          </li>
-          <li>
-            <img src="/img/home/activity02.jpg" alt="" />
-          </li>
-        </ul>
-      </div>
-      <div className="ning_ticketbox">
-        <div className="ning_Admissionbox"></div>
-        <div className="ning_AdmissionboxIcon">
-          <i className="homeIconfas fas fa-users"></i>
-          <p className="ninghomeAdmissionText">入園人數</p>
+      <div className="ning_homecontainer">
+        <div className="ning_CarouselWrap">
+          <ul className="ning_CarouselWrapImg">
+            <li>
+              <img src="/img/home/activity03.jpg" alt="" />
+            </li>
+            <li>
+              <img src="/img/home/activity01.jpeg" alt="" />
+            </li>
+            <li>
+              <img src="/img/home/activity02.jpg" alt="" />
+            </li>
+          </ul>
         </div>
-        <div className="ninghomeAdmissionNumber">180</div>
-      </div>
-      {/* <div className="ning_JungleTour">
+        <div className="ning_ticketbox">
+          <div className="ning_Admissionbox">
+            <div className="ning_AdmissionboxIcon">
+              <span className="material-icons">groups</span>
+              <p className="ninghometicket">入園人數</p>
+              <p className="ninghometicketEn">Ticket</p>
+            </div>
+            <div className="ninghomeAdmissionCout">
+              <div className="ninghomeAdmissionNumber">180</div>
+              <p className="ninghomeAdmissionUnit">人</p>
+            </div>
+          </div>
+        </div>
+        {/* <div className="ning_JungleTour">
         <div className="ning_bgcblack"><img src="/img/home/homebgcblack.svg" alt="" /></div>
         <div className="ning_JungleTourTitle">
           <h1 className="ning_EnTitle">Jungle Tour</h1>
@@ -37,8 +43,7 @@ function homePage() {
           <img src="/img/home/tropical.jpg" alt="" />
         </div>
       </div> */}
-
-      
+      </div>
     </>
   );
 }
