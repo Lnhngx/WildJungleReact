6efe38d2cbf93e2d321075685e2561b05ab4f923
Stan Lin <love4210kimo@gmail.com>
2022-03-10 15:23:57 +0800

    Update Product area , RWD and scss

--- src/pages/carts/carts.js
+++ src/pages/carts/carts.js
@@ -6,13 +6,20 @@ function carts() {
   return (
     <>
       <div className="stan_carts_main">
-      <Process01 />
-      <div className="stan_cart_products">
-        123
+        <Process01 />
+        <div className="stan_carts_middle">
+          <div className="stan_product_area">
+              <ul className="stan_product_selector">
+                <li className="stan_selector_Item">商品清單</li>
+                <li className="stan_selector_Item">票券</li>
+                <li className="stan_selector_Item">認養</li>
+                <li className="stan_selector_Item">預定住宿資訊</li>
+              </ul>
+              <hr />
+          </div>
+          <div className="stan_checkout_area">checkout_area</div>
+        </div>
       </div>
-      </div>
-
-
     </>
   );
 }
