bc7e4c43c6d65216f6131a95dd0d453b3c8cc33a
Yi-Ning <ning19950212@gmail.com>
2022-03-12 03:32:40 +0800

    jsx

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -3,119 +3,142 @@ import "./ning.scss";
 function homePage() {
   return (
     <>
-      <div className="ning_homecontainer">
-        <div className="ning_homecarouselwrap">
-          <div className="ning_homecarouselwrapimg">
-            <div>
-              <a href="#/">
-                <img src="/img/home/activity03.jpg" alt="" />
-              </a>
+      <div className="ning_homecarouselwrap">
+        <ul className="ning_homecarouselwrapimg">
+          <li>
+            <a href="#/">
+              <img src="/img/home/activity03.jpg" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#/">
+              <img src="/img/home/activity01.jpeg" alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#/">
+              <img src="/img/home/activity02.jpg" alt="" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="ning_informationbox">
+        <div className="ning_information">
+          <div className="ning_Admissionbox">
+            <div className="ning_AdmissionboxIcon">
+              <div className="ning_iconsbox">
+                <span className="material-icons">groups</span>
+              </div>
+              <p className="ning_Admission">入園人數</p>
+              <p className="ning_AdmissionEn">Ticket</p>
             </div>
-            <div>
-              <a href="#/">
-                <img src="/img/home/activity01.jpeg" alt="" />
-              </a>
+            <div className="ning_AdmissionCout">
+              <div className="ning_AdmissionNumber">180</div>
+              <p className="ning_AdmissionUnit">人</p>
             </div>
-            <div>
-              <a href="#/">
-                <img src="/img/home/activity02.jpg" alt="" />
-              </a>
+          </div>
+          <div className="ning_separationline"></div>
+          <div className="ning_weatherbox">
+            <div className="ning_weatherboxIcon">
+              <div className="ning_weathericon">
+                <span className="material-icons">cloud</span>
+              </div>
+              <p className="ning_weather">現在天氣</p>
+              <p className="ning_weatherEn">Wether</p>
+            </div>
+            <div className="ning_weatherCout">
+              <div className="ning_weatherNumber">18</div>
+              <p className="ning_weatherUnit">°C</p>
+            </div>
+          </div>
+          <div className="ning_separationline"></div>
+          <div className="ning_ticketbox">
+            <div className="ning_ticketboxIcon">
+              <div className="ning_ticketicon">
+                <span class="material-icons">confirmation_number</span>
+              </div>
+              <p className="ning_ticket">票券資訊</p>
+              <p className="ning_ticketEn">Ticket</p>
+            </div>
+            <div className="ning_ticketwrap">
+              <div className="ning_aldultbox">
+                <div className="ning_aldult">全票</div>
+                <p className="ning_aldultprice">$50</p>
+              </div>
+              <div className="ning_studentbox">
+                <div className="ning_student">學生票</div>
+                <p className="ning_studentprice">$30</p>
+              </div>
+              <div className="ning_lovebox">
+                <div className="ning_love">愛心票</div>
+                <p className="ning_loveprice">$20</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="ning_informationbox"></div>
-        {/* <div className="ning_informationbox">
-          <div className="ning_Admissionbox">
-            <div className="ning_AdmissionboxIcon">
-              <span className="material-icons">groups</span>
-              <p className="ninghomeAdmission">入園人數</p>
-              <p className="ninghomeAdmissionEn">Ticket</p>
-            </div>
-            <div className="ninghomeAdmissionCout">
-              <div className="ninghomeAdmissionNumber">180</div>
-              <p className="ninghomeAdmissionUnit">人</p>
-            </div>
-            <div className="ning_ticketborderbox">
-              <div className="ning_ticketborder"></div>
-            </div>
-          </div>
-          <div className="ning_weatherbox">
-            <div className="ning_weatherboxIcon">
-              <i class="ning_weatherIcon fas fa-cloud"></i>
-              <p className="ninghomeweather">現在天氣</p>
-              <p className="ninghomeweatherEn">Wether</p>
-            </div>
-            <div className="ninghomeweatherCout">
-              <div className="ninghomeweatherNumber">18</div>
-              <p className="ninghomeweatherUnit">°C</p>
-            </div>
-            <div className="ning_ticketborderbox">
-              <div className="ning_ticketborder"></div>
-            </div>
-          </div>
-          <div className="ning_ticketbox">
-            <div className="ning_ticketboxIcon">
-              <span class="ning_ticketIcon material-icons">
-                confirmation_number
-              </span>
-              <p className="ninghometicket">票券資訊</p>
-              <p className="ninghometicketEn">Ticket</p>
-            </div>
-            <div className="ninghomealdultbox">
-              <div className="ninghomealdult">全票</div>
-              <p className="ninghomealdultprice">$50</p>
-            </div>
-            <div className="ninghomestudentbox">
-              <div className="ninghomestudent">學生票</div>
-              <p className="ninghomestudentprice">$30</p>
-            </div>
-            <div className="ninghomelovebox">
-              <div className="ninghomelove">愛心票</div>
-              <p className="ninghomeloveprice">$20</p>
-            </div>
-          </div>
-        </div> */}
-        {/* <div className="ning_bgcblack">
-          <img src="/img/home/homebgcblack.svg" alt="" />
+      </div>
+      <div className="ning_bgcblack">
+        <div className="container">
           <div className="ning_JungleTour">
-          <h1 className="ning_EnTitle">Jungle Tour</h1>
-          <p className="ning_ChTitle">園區分類一覽</p>
-          <div className="ning_JungleTourbox">
-            <div className="ning_JungleTourmap">
-              <img src="/img/home/tropicalmap.svg" alt="" />
-            </div>
-            <div className="ning_hometourimgbox">
-              <div className="ning_hometropical">
-                <img
-                  className="ning_hometropicalimg"
-                  src="/img/home/tropical.jpg"
-                  alt=""
-                />
+            <h1 className="ning_TitleEn">Jungle Tour</h1>
+            <h2 className="ning_TitleCh">園區分類一覽</h2>
+          </div>
+        </div>
+        <div className="ning_JungleTourmapbox">
+          <div className="ning_JungleTourmap">
+            <img src="/img/home/tropicalmap.svg" alt="" />
+          </div>
+          <div className="ning_Jungletourimgbox">
+            <a href="#/">
+              <div className="ning_Jungletropical">
+                <div className="ning_tourboxhd">
+                  <img
+                    className="ning_Jungletropicalimg"
+                    src="/img/home/tropical.jpg"
+                    alt=""
+                  />
+                </div>
                 <p>熱帶雨林館</p>
               </div>
-              <div className="ning_homeOcean">
-                <img
-                  className="ning_homeOceanimg"
-                  src="/img/home/Ocean.jpg"
-                  alt=""
-                />
+            </a>
+            <a href="#/">
+              <div className="ning_JungleOcean">
+                <div className="ning_tourboxhd">
+                  <img
+                    className="ning_JungleOceanimg"
+                    src="/img/home/Ocean.jpg"
+                    alt=""
+                  />
+                </div>
                 <p>海底世界館</p>
               </div>
-              <div className="ning_homenocturnal">
-                <img
-                  className="ning_homenocturnalimg"
-                  src="/img/home/nocturnal.jpg"
-                  alt=""
-                />
+            </a>
+            <a href="#/">
+              <div className="ning_Junglenocturnal">
+                <div className="ning_tourboxhd">
+                  <img
+                    className="ning_Junglenocturnalimg"
+                    src="/img/home/nocturnal.jpg"
+                    alt=""
+                  />
+                </div>
                 <p>夜行動物館</p>
               </div>
-              <div className="ning_homeice">
-                <img className="ning_homeiceimg" src="/img/home/ice.jpg" alt="" />
+            </a>
+            <a href="#/">
+              <div className="ning_Jungleice">
+                <div className="ning_tourboxhd">
+                  <img
+                    className="ning_Jungleiceimg"
+                    src="/img/home/ice.jpg"
+                    alt=""
+                  />
+                </div>
                 <p>冰原歷險館</p>
               </div>
-            </div>
+            </a>
           </div>
-        </div> */}
+        </div>
       </div>
     </>
   );
