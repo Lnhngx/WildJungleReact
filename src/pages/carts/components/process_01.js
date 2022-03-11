bfc4d2a0de6cf1374163ecf654b653d504c1aaf5
Stan Lin <love4210kimo@gmail.com>
2022-03-12 02:36:11 +0800

    Update carts and scss

--- src/pages/carts/components/process_01.js
+++ src/pages/carts/components/process_01.js
@@ -3,13 +3,15 @@ import React from "react";
 function Process01() {
   return (
     <>
-      <ul className="stan_process_iconarea">
-        <li className="stan_circle stan_word1">1</li>
-        <li className="stan_line"></li>
-        <li className="stan_circle stan_word2">2</li>
-        <li className="stan_line"></li>
-        <li className="stan_circle stan_word3">3</li>
-      </ul>
+      <div className="stan_process_iconarea">
+        <ul>
+          <li>1</li>
+          <li></li>
+          <li>2</li>
+          <li></li>
+          <li>3</li>
+        </ul>
+      </div>
     </>
   );
 }
