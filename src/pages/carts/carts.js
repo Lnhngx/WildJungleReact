1675700ef4776f8b645880bbf9116f6fa8435edb
Stan Lin <love4210kimo@gmail.com>
2022-03-13 00:03:00 +0800

    更新購物車第一頁(95%完成)

--- src/pages/carts/carts.js
+++ src/pages/carts/carts.js
@@ -17,7 +17,7 @@ function carts() {
               <li>認養</li>
               <li>預定住宿</li>
             </ul>
-            <hr className="stan_hr" />
+            <div className="stan_hr"></div>
             <ul className="stan_forcus_selector">
               <li></li>
               <li></li>
@@ -26,7 +26,7 @@ function carts() {
             </ul>
             <div className="stan_product_title">
               <ul>
-                <li>Empty</li>
+                <li></li>
                 <li>商品種類</li>
                 <li>商品名稱</li>
                 <li>單價</li>
@@ -38,11 +38,23 @@ function carts() {
               <ul >
                 <li>X</li>
                 <li>這是圖片</li>
-                <li>法蘭絨動物兒童披風</li>
-                <li>$900</li>
-                <li>按鈕</li>
+                <li className="stan_ds_product">法蘭絨動物兒童披風</li>
+                <li className="stan_ds_product">$900</li>
+                <li className="stan_ds_product">按鈕</li>
+                <div className="stan_mo_product">
+                  <li>法蘭絨動物兒童披風</li>
+                  <li>$900</li>
+                  <li>按鈕</li>
+                </div>
               </ul>
               <div className="stan_blackhr"></div>
+            </div>
+            <div className="stan_product_total">
+              <ul>
+                <li></li>
+                <li>小計</li>
+                <li>$3,000</li>
+              </ul>
             </div>
           </div>
           <div className="stan_block"></div>
