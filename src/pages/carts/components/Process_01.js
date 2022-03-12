39adebf30f9e4461ccac4666778311a62d3750d5
Stan Lin <love4210kimo@gmail.com>
2022-03-12 08:06:00 +0800

    重來一下

---
+++ src/pages/carts/components/Process_01.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Process01() {
+  return (
+    <>
+      <div className="stan_process_iconarea">
+        <ul>
+          <li>1</li>
+          <li></li>
+          <li>2</li>
+          <li></li>
+          <li>3</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Process01;
