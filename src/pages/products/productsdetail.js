12e5cc8a896b27abf160e2d5a6a03e299683ecb1
Lnhngx <a8817388064@gmail.com>
2022-03-14 17:58:59 +0800

    product

--- src/pages/products/productsdetail.js
+++ src/pages/products/productsdetail.js
@@ -11,80 +11,94 @@ function ProductsDetail() {
           width: 0 !important;
         }
       `}</style>
-      <Productsbackground />
+      <div className="alan_bread">
+        <ul className="alan_navbread">
+          <li>
+            <Link to="">
+              <div className="alan_return"></div>
+            </Link>
+          </li>
+          <li>
+            <Link to="">Item1 |</Link>
+          </li>
+          <li>
+            <Link to="">Item2 |</Link>
+          </li>
+          <li>
+            <Link to="">Item3 |</Link>
+          </li>
+          <li>
+            <Link to="">Item4 |</Link>
+          </li>
+          <li>
+            <Link to="">Item5 |</Link>
+          </li>
+          <li>
+            <Link to="">Item6</Link>
+          </li>
+        </ul>
+      </div>
       <div className="alan_detail">
-        <div className="alan_bread">
-          <ul className="alan_navbread">
-            <li>
-              <Link to="">
-                <div className="alan_return"></div>
-              </Link>
+        <div className="alan_productsdetail">
+          <div className="alan_main_product_img">
+            <img src={require("./imgs/cloth-2 3.png")} alt="" />
+          </div>
+          <ul className="alan_product_img_ul">
+            <li className="alan_product_img_li">
+              <img src={require("./imgs/cloth-2 3.png")} alt="" />
             </li>
-            <li>
-              <Link to="">Item1 |</Link>
+            <li className="alan_product_img_li">
+              <img src={require("./imgs/cloth-3.png")} alt="" />
             </li>
-            <li>
-              <Link to="">Item2 |</Link>
+            <li className="alan_product_img_li">
+              <img src={require("./imgs/cloth-4.png")} alt="" />
             </li>
-            <li>
-              <Link to="">Item3 |</Link>
+            <li className="alan_product_img_li">
+              <img src={require("./imgs/cloth-5.png")} alt="" />
             </li>
-            <li>
-              <Link to="">Item4 |</Link>
-            </li>
-            <li>
-              <Link to="">Item5 |</Link>
-            </li>
-            <li>
-              <Link to="">Item6</Link>
+            <li className="alan_product_img_li">
+              <img src={require("./imgs/cloth-6.png")} alt="" />
             </li>
           </ul>
-        </div>
-        <div className="alan_productsdetail">
-          <div className="alan_product_img">
-            <ul className="alan_product_img_ul">
-              <li className="alan_product_img_li">
-                <img src={require("./imgs/cloth-2 3.png")} alt="" />
-              </li>
-              <li className="alan_product_img_li">
-                <img src={require("./imgs/cloth-3.png")} alt="" />
-              </li>
-              <li className="alan_product_img_li">
-                <img src={require("./imgs/cloth-4.png")} alt="" />
-              </li>
-              <li className="alan_product_img_li">
-                <img src={require("./imgs/cloth-5.png")} alt="" />
-              </li>
-              <li className="alan_product_img_li">
-                <img src={require("./imgs/cloth-6.png")} alt="" />
-              </li>
-            </ul>
-            <div className="alan_product_img_icon">01/06</div>
-          </div>
-        </div>
-        <div className="alan_product_star">
-          <div className="alan_star">
-            <StarRating />
-          </div>
-          <div className="alan_comment">
-            <Link to="">
-              <span>發表評論</span>
-            </Link>
-          </div>
+          <div className="alan_product_img_icon">01/06</div>
         </div>
         <div className="alan_productTitle">
+          <div className="alan_product_star">
+            <div className="alan_star">
+              <StarRating />
+            </div>
+            <div className="alan_comment">
+              <Link to="">
+                <span>發表評論</span>
+              </Link>
+            </div>
+          </div>
           <div className="alan_productName">
             <span className="alan_englishName">Kid’s poncho</span>
             <span className="alan_chineseName">保暖小獅王手套</span>
           </div>
           <div className="alan_price">
+            <div className="alan_product_reserve">
+              <span>商品規格:100cm*100cm*100cm</span>
+              <span>庫存餘剩99個</span>
+            </div>
             <span className="alan_product_price">$790</span>
           </div>
           <div className="alan_productButtonGroup">
-            <div className="alan_quantity">
-              <button>-</button>
-              <span>1</span>
-              <button>+</button>
+            <div className="alan_quantityGroup">
+              <div className="alan_quantity">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
+              <div className="alan_hashlikedesk">
+                <Link to="">
+                  <i className="fas fa-heart"></i> 加入我的最愛
+                </Link>
+                <Link to="">
+                  <i className="fas fa-plus"></i> 加入商品比較
+                </Link>
+              </div>
             </div>
             <div className="alan_buy">
               <button>
@@ -119,12 +133,17 @@ function ProductsDetail() {
               </div>
             </div>
             <div className="alan_hushlike">
-                <Link to=""><i className="fas fa-heart"></i> 加入我的最愛</Link>
-                <Link to=""><i className="fas fa-plus"></i> 加入商品比較</Link>
+              <Link to="">
+                <i className="fas fa-heart"></i> 加入我的最愛
+              </Link>
+              <Link to="">
+                <i className="fas fa-plus"></i> 加入商品比較
+              </Link>
             </div>
           </div>
         </div>
       </div>
+      <Productsbackground />
     </>
   );
 }
