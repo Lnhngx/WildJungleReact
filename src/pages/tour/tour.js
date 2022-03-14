34202d33f32afe02520feb0ab62b5683642f4ddb
TingYun <tingyun02@hotmail.com>
2022-03-14 14:41:18 +0800

    css

--- src/pages/tour/tour.js
+++ src/pages/tour/tour.js
@@ -3,8 +3,55 @@ import "./tour.scss";
 
 function tour(){
     return(<>
+
+
+
+
     <div className="tourbackground"></div>
     <div className="tourContainer">
+
+    <div style={{display: 'flex',paddingTop:"5rem" ,flexWrap:"wrap"}} className="tysu_Group">
+        <div style={{maxWidth:"30%"}} className="tysu_tourGuide1">
+            <div style={{color: 'white'}}>
+                <h2 className="tysu_tourH2" style={{fontSize:"6rem"}}>Jungle Tour</h2>
+                <p className="tysu_tour" style={{fontSize:"3rem"}}>園區導覽</p>
+            </div>
+            <div>
+            <img  style={{width:"100%"}} src="/img/tour/Group 277.png" alt="" />
+            </div>
+        </div>
+
+        <div style={{width:"70%", display: 'flex',color: 'white'}} className="tysu_tourGuide2">
+            <figure style={{height:"40vw",maxWidth:"25%" ,outline: '1px solid red',textAlign: 'center'}}>
+              <img  style={{width:"100%",height:"100%",objectFit: 'cover' ,objectPosition:"35%"}} src="/img/home/tropical.jpg" alt="#/" /> 
+              <figcaption>海底世界</figcaption>
+            </figure>
+            <figure style={{height:"40vw",maxWidth:"25%" ,outline: '1px solid red',textAlign: 'center'}}>
+              <img  style={{width:"100%",height:"100%",objectFit: 'cover' ,objectPosition:"70%"}} src="/img/home/Ocean.jpg" alt="#/" /> 
+              <figcaption>海底世界</figcaption>
+            </figure>
+            <figure style={{height:"40vw",maxWidth:"25%" ,outline: '1px solid red',textAlign: 'center'}}>
+              <img  style={{width:"100%",height:"100%",objectFit: 'cover' ,objectPosition:"68%"}} src="/img/home/nocturnal.jpg" alt="#/" />
+              <figcaption>海底世界</figcaption>
+            </figure>
+            <figure style={{height:"40vw",maxWidth:"25%" ,outline: '1px solid red',textAlign: 'center'}}>
+              <img  style={{width:"100%",height:"100%",objectFit: 'cover' ,objectPosition:"80%"}} src="/img/home/ice.jpg" alt="#/" /> 
+              <figcaption>海底世界</figcaption>
+            </figure>
+    </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
       <div className="tourTop">
         <div className="tourLeftIndustry">
           <div className="tourContent">
@@ -15,11 +62,13 @@ function tour(){
             <img src="/img/tour/Group 277.png" alt="" />
           </div>
         </div>
-        <div className="tourRightIndustry">
+      </div>
+
+    
+      <div className="tourRightIndustry">
             <figure className="tourR1img">
               <img src="/img/home/tropical.jpg" alt="#/" /> 
               <figcaption>海底世界</figcaption>
-             
             </figure>
            
             
@@ -35,8 +84,10 @@ function tour(){
               <img src="/img/tour/Sphenisciformes/90e80dbb-penguin-chick.jpeg" alt="#/" />
               <figcaption>極地世界</figcaption>
             </figure>
-        </div>
       </div>
+
+
+
       <div className="Container" id="container2">
         <div className="tourCenterIndustry">
           <div className="tourCenterCard" id="Centerimg1">
