124a796e87846a905acda49249f08e6401f3994d
tony-hsueh <yahooleo36@gmail.com>
2022-03-14 15:03:28 +0800

    fix

--- src/pages/game/multiChoice-game.js
+++ src/pages/game/multiChoice-game.js
@@ -1,6 +1,5 @@
 import React from "react";
-import './spot-diff.css';
-
+import './multiChoice-game.css';
 function MultiChoice(){
     return(
     <>
@@ -25,10 +24,20 @@ function MultiChoice(){
             </div>
             <div className="game_title">冷知識大挑戰</div>
             <p className="game_profile">遊戲中你將要找出<span>5</span>個不同之處，利用滑鼠點擊圖片若正確會看到綠色圈圈。<br/>記住.....你只有<span>3</span>次錯誤的機會。<br/>抓緊時間吧，完成考驗將可以得到<span>紅利點數1000 !!!</span></p>
-            <div className="container_game">
-                <div className="game_zone">
-                    
-                    
+            <div className="mc_container_game">
+                <div className="mc_game_border">
+                    <div className="mc_game_zone">
+                        <h2>1.請問斑馬的皮膚是什麼顏色?</h2>
+                        <div className="mc_btnGroup">
+                            <button className="right">黑白相間</button>
+                            <button>白色</button>
+                            <button>黑色</button>
+                            <button>灰色</button>
+                        </div>
+                        <div className="mc_help">
+                            <i className="fas fa-question"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="animals-footer"> 
