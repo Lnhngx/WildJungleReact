be140cff5fdd1ef6488021ae85b0e76f12faab0e
Yi-Ning <ning19950212@gmail.com>
2022-03-16 00:29:25 +0800

    scss

--- src/pages/homePage/components/HomeTransportion.js
+++ src/pages/homePage/components/HomeTransportion.js
@@ -1,28 +1,128 @@
 import React from "react";
 
-const HomeAccordion = () => {
+const HomeTransportion = () => {
   return (
     <>
-      <div className="ning_transportionButton">
-        <button className="ning_busButton btn">公車即時</button>
-        <button className="ning_PButton btn">停車場</button>
-        <h3 className="ning_busstopname">捷運大安站(信義)</h3>
+      <h3 className="ning_busstopname">捷運大安站(信義)</h3>
+      <div className="ning_transportionbox">
         <div className="ning_busbox">
-          <div className="ning_busbox1">
-            <div className="ning_bus">
-              <p className="ning_bustime">
-                4<span>分鐘</span>
-              </p>
-              <p className="ning_busname">
-                信義幹線<br/>
-                <span>往臺北車站</span>
-              </p>
-            </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
           </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+          <div className="ning_bus">
+            <p className="ning_bustime">
+              4<span>分鐘</span>
+            </p>
+            <p className="ning_busname">
+              信義幹線
+              <br />
+              <span>往臺北車站</span>
+            </p>
+          </div>
+        </div>
+        <div className="ning_busmap">
+          <img src="img/home/busmap.jpg" alt="" />
         </div>
       </div>
     </>
   );
 };
 
-export default HomeAccordion;
+export default HomeTransportion;
