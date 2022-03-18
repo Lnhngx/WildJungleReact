05bff390d918cd1ed0cbd24a8316c088c5fd1887
Stan Lin <love4210kimo@gmail.com>
2022-03-18 15:56:05 +0800

    Update

--- src/pages/carts/carts.js
+++ src/pages/carts/carts.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 import Carts01product from "./carts_01_product";
 import Carts02filloutform from "./carts_02_filloutform";
 import Carts03finishorder from "./carts_03_finishorder";
+
+
+
+const products = [
+  {
+    id: 1,
+    image: "https://i.imgur.com/1GrakTl.jpg",
+    name: "咖啡色 T-shirt",
+    price: 300,
+  },
+  {
+    id: 2,
+    image: "https://i.imgur.com/ba3tvGm.jpg",
+    name: "白色 T-shirt",
+    price: 200,
+  },
+  {
+    id: 3,
+    image: "https://i.imgur.com/pHQ3xT3.jpg",
+    name: "黑色 T-shirt",
+    price: 450,
+  },
+  {
+    id: 4,
+    image: "https://i.imgur.com/pHQ3xT3.jpg",
+    name: "金色 T-shirt",
+    price: 100,
+  },
+];
 
 function carts() {
   return (
--- src/pages/carts/carts_01_product.js
+++ src/pages/carts/carts_01_product.js
@@ -4,33 +4,6 @@ import Process01 from "./components/Process_01";
 import CartProduct from "./components/CartProduct";
 import Checkarea from "./components/Checkarea";
 
-// const products = [
-//   {
-//     id: 1,
-//     image: "https://i.imgur.com/1GrakTl.jpg",
-//     name: "咖啡色 T-shirt",
-//     price: 300,
-//   },
-//   {
-//     id: 2,
-//     image: "https://i.imgur.com/ba3tvGm.jpg",
-//     name: "白色 T-shirt",
-//     price: 200,
-//   },
-//   {
-//     id: 3,
-//     image: "https://i.imgur.com/pHQ3xT3.jpg",
-//     name: "黑色 T-shirt",
-//     price: 450,
-//   },
-//   {
-//     id: 4,
-//     image: "https://i.imgur.com/pHQ3xT3.jpg",
-//     name: "金色 T-shirt",
-//     price: 100,
-//   },
-// ];
-
 function Carts01product() {
   return (
     <>
