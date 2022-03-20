c1817c963a0857db85bcef25ee8bd7f016bec49d
Lnhngx <a8817388064@gmail.com>
2022-03-20 14:19:22 +0800

    123

---
+++ src/pages/products/components/productCard.js
@@ -0,0 +1,189 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ProductCard() {
+  return (
+    <>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="productCard">
+        <Link to="">
+          <div className="cardImg">
+            <img src="img/product/cloth-2.png" alt="" />
+          </div>
+        </Link>
+        <div className="cardName">
+          <Link className="cardTitle" to="">
+            <p>法蘭絨動物兒童披風</p>
+            <span>Animal Moco</span>
+            <span>$799</span>
+          </Link>
+          <div className="cardIcon">
+            <Link to="">
+              <i className="fas fa-heart"></i>
+            </Link>
+            <Link to="">
+              <i className="fas fa-shopping-cart"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ProductCard;
