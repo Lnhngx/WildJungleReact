7585dc646ca242c2a5b5e1b0444ff09554b8974d
TingYun <tingyun02@hotmail.com>
2022-03-22 01:23:28 +0800

    test state

--- src/pages/members/components/MemberList copy.js
+++ src/pages/members/components/MemberList copy.js
@@ -9,16 +9,8 @@ import MemberNavItem from './MemberNavItem'
 function MemberList(){
   const memberlist=['會員資料','訂單資訊','折價優惠','喜愛收藏']
 
-  const navItem=['基本設定','分級資訊','信用卡管理','常用資訊']
-  const orderNavItem=['訂單查詢','票券查詢','訂單退換貨','配送設定']
-  const discountNavItem=['紅利','折價券']
-  const likeNavItem=['商品','活動']
-
-  // 紀錄memberList的狀態
   const [actived,setActived]=useState('會員資料')
-  // const [navAct,setNavAct]=useState({navItem:'基本設定',orderNavItem:'訂單查詢',discountNavItem:'紅利',likeNavItem:'商品'})
-  // 紀錄navItem狀態
-  const [navState,setNavState]=useState('基本設定')
+  const [navAct,setNavAct]=useState({navItem:'基本設定',orderNavItem:'訂單查詢',discountNavItem:'紅利',likeNavItem:'商品'})
 
   return(
     
@@ -29,16 +21,13 @@ function MemberList(){
       <div className="tysu_row">
           <ul className="tysu_memberList">
             {memberlist.map((v,i)=>{
-              return <li key={i} onClick={(e)=>{
+              return <li key={i} onClick={()=>{
+                setNavAct(navAct)
                 setActived(v)
-                setNavState(navItem[0])
               }} className={actived===v ?'active':''}><Link to="#/"  className="tysu_link">{v}</Link></li>
             })}
           </ul>
-        <MemberNavItem memberlist={memberlist} navItem={navItem}  orderNavItem={orderNavItem} actived={actived} discountNavItem={discountNavItem} likeNavItem={likeNavItem} 
-          setNavState={setNavState}
-          navState={navState}
-        />
+        <MemberNavItem memberlist={memberlist} actived={actived} navAct={navAct}/>
         <div className="tysu_memberBg">
           <img src="./../img/member/leaf_y.svg" alt="" />
         </div>
--- src/pages/members/components/MemberNavItem copy.js
+++ src/pages/members/components/MemberNavItem copy.js
@@ -15,7 +15,7 @@ import ProductLike from './ProductLike'
 import ActivityLike from './ActivityLike'
 
 function MemberNavItem(props){
-  const {memberlist,actived,setNavState,navState}=props
+  const {memberlist,actived,navAct}=props
 
   const [navActived,setNavActived]=useState({navItem:'基本設定',orderNavItem:'訂單查詢',discountNavItem:'紅利',likeNavItem:'商品'})
   const navItem=['基本設定','分級資訊','信用卡管理','常用資訊']
@@ -23,51 +23,42 @@ function MemberNavItem(props){
   const discountNavItem=['紅利','折價券']
   const likeNavItem=['商品','活動']
   
-  const [underLine,setUnderLine]=useState()
+
 
 
   return(<>
   <ul className="tysu_memberChild">
     {/* props傳入的對象是誰，就渲染哪個nav item；除被點擊的nav item以外，其他nav item變更回原狀態 */}
     {actived===memberlist[0] ? navItem.map((v,i)=>{
-
-      
-
-      return <li key={i} onClick={(e)=>{
-        // console.log(e.target.innerHTML)
-        if(e.target.innerHTML===v){
-          setNavActived({...navActived,navItem:v})
-          setNavState(e.target.innerHTML)
-        }
-      }}>
-                <Link to="#"  className="tysu_link">{v}</Link>
-              </li>
-            }
+      return <li key={i} onClick={()=>{
+        setNavActived({...navActived,navItem:v,orderNavItem:orderNavItem[0],discountNavItem:discountNavItem[0],likeNavItem:likeNavItem[0]})
+        }}>
+              <Link to="#"  className="tysu_link">{v}</Link></li>
+              }
     ):''}
-   
     {actived===memberlist[1] && orderNavItem.map((v,i)=>{
-      return <li key={i}>
-                <Link to="#"  className="tysu_link">{v}</Link>
-              </li>
-            }
+      return <li key={i} onClick={()=>{
+        setNavActived({...navActived,orderNavItem:v,navItem:navItem[0],discountNavItem:discountNavItem[0],likeNavItem:likeNavItem[0]})}}>
+              <Link to="#"  className="tysu_link">{v}</Link></li>
+              }
     )}
     {actived===memberlist[2] && discountNavItem.map((v,i)=>{
-    return <li key={i}>
-              <Link to="#"  className="tysu_link">{v}</Link>
-            </li>
+    return <li key={i}  onClick={()=>{
+      setNavActived({...navActived,discountNavItem:v,navItem:navItem[0],orderNavItem:orderNavItem[0],likeNavItem:likeNavItem[0]})}}>
+            <Link to="#"  className="tysu_link">{v}</Link></li>
             }
     )}
     {actived===memberlist[3] && likeNavItem.map((v,i)=>{
-    return <li key={i} >
-              <Link to="#"  className="tysu_link">{v}</Link>
-            </li>
+    return <li key={i} onClick={()=>{
+      setNavActived({...navActived,likeNavItem:v,navItem:navItem[0],orderNavItem:orderNavItem[0],discountNavItem:discountNavItem[0]})}}>
+            <Link to="#"  className="tysu_link">{v}</Link></li>
             }
     )}
   </ul>
   <hr className="tysu_hr" />
   <ul className="tysu_focusUnderLine tysu_memberChild">
-    {  actived===memberlist[0] && navItem.map((v,i)=>{
-      return <Link to="#" key={i} className={navState===v ? 'tysu_link':'tysu_link disabled' }><li></li></Link>
+    {actived===memberlist[0] && navItem.map((v,i)=>{
+      return <Link to="#" key={i} className={navActived.navItem===v ? 'tysu_link' :'tysu_link disabled'}><li></li></Link>
     })}
     {actived===memberlist[1] && orderNavItem.map((v,i)=>{
       return <Link to="#" key={i} className={navActived.orderNavItem===v ? 'tysu_link' :'tysu_link disabled'}><li></li></Link>
