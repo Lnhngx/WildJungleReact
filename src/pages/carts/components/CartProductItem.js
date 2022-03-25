08b7492d882d51608d441140c5dcf63bf382d1d7
Stan Lin <love4210kimo@gmail.com>
2022-03-25 17:48:42 +0800

    修改中

--- src/pages/carts/components/CartProductItem.js
+++ src/pages/carts/components/CartProductItem.js
@@ -1,63 +1,66 @@
 import React from "react";
+import { useCart } from "../utils/useCart";
 
-function CartProductItem(props) {
-  const { sid, image, name, price, quantity, setQuantity } = props;
+function CartProductItem() {
+  const { items, plusOne, minusOne, removeItem } = useCart();
   return (
     <>
-      <div className="stan_product_space">
-        <ul>
-          <li>X</li>
-          <li>
-            <img alt="" src={image} />
-          </li>
-          <li className="stan_ds_product">{name}</li>
-          <li className="stan_ds_product">${price}</li>
-          <li className="stan_ds_product stan_count_btn">
-            <button
-              className="stan_count_minus"
-              onClick={() => {
-                setQuantity(quantity - 1);
-              }}
-            >
-              <i className="fas fa-minus stan_btnicon"></i>
-            </button>
-            <div className="stan_btninput">{quantity}</div>
-            <button
-              className="stan_count_add"
-              onClick={() => {
-                setQuantity(quantity + 1);
-              }}
-            >
-              <i className="fas fa-plus stan_btnicon"></i>
-            </button>
-          </li>
-
-          <div className="stan_mo_product">
-            <li>{name}</li>
-            <li>${price}</li>
-            <li className="stan_count_btn">
+      {items.map((v, i) => {
+        <div className="stan_product_space">
+          <ul>
+            <li><i class="fas fa-times"></i></li>
+            <li>
+              <img alt="" src={v.image} />
+            </li>
+            <li className="stan_ds_product">{v.name}</li>
+            <li className="stan_ds_product">${v.price}</li>
+            <li className="stan_ds_product stan_count_btn">
               <button
                 className="stan_count_minus"
                 onClick={() => {
-                  setQuantity(quantity - 1);
+                  minusOne(v.sid)
                 }}
               >
                 <i className="fas fa-minus stan_btnicon"></i>
               </button>
-              <div className="stan_btninput">{quantity}</div>
+              <div className="stan_btninput">{v.quantity}</div>
               <button
                 className="stan_count_add"
                 onClick={() => {
-                  setQuantity(quantity + 1);
+                  plusOne(v.sid)
                 }}
               >
                 <i className="fas fa-plus stan_btnicon"></i>
               </button>
             </li>
-          </div>
-        </ul>
-        <div className="stan_blackhr"></div>
-      </div>
+
+            <div className="stan_mo_product">
+              <li>{v.name}</li>
+              <li>${v.price}</li>
+              <li className="stan_count_btn">
+                <button
+                  className="stan_count_minus"
+                  onClick={() => {
+                    minusOne(v.sid)
+                  }}
+                >
+                  <i className="fas fa-minus stan_btnicon"></i>
+                </button>
+                <div className="stan_btninput">{v.quantity}</div>
+                <button
+                  className="stan_count_add"
+                  onClick={() => {
+                    plusOne(v.sid)
+                  }}
+                >
+                  <i className="fas fa-plus stan_btnicon"></i>
+                </button>
+              </li>
+            </div>
+          </ul>
+          <div className="stan_blackhr"></div>
+        </div>;
+      })}
     </>
   );
 }
