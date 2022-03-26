b2132ffc46fb566dc621f7ff396fb812bccd2bb7
TingYun <tingyun02@hotmail.com>
2022-03-26 16:50:55 +0800

    login&out

--- src/pages/members/Config.js
+++ src/pages/members/Config.js
@@ -3,6 +3,7 @@ const host='http://localhost:4000';
 const Config={
     TYSU_LOGIN:`${host}/members/login`,
     TYSU_SIGNUP:`${host}/members/signup`,
+    TYSU_LOGOUT:`${host}/members/logout`,
     TYSU_FORGOT_PASS:`${host}/members/forgotpass`,
     TYSU_MEMBER_INFO:`${host}/members/edit/`,
 }
