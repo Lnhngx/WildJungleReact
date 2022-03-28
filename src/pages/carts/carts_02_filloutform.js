98b4aaa9dd6b5763dc92d34f5964da69fd6ee2d2
Stan Lin <love4210kimo@gmail.com>
2022-03-28 20:15:50 +0800

    e04

--- src/pages/carts/carts_02_filloutform.js
+++ src/pages/carts/carts_02_filloutform.js
@@ -3,10 +3,11 @@ import "./carts.scss";
 import Process02 from "./components/Process_02";
 import Filloutorder from "./components/Filloutform_order";
 import { useCart } from "./utils/useCart";
-import Deliveryhome from "./components/Delivery_home";
-import Deliveryconvenient from "./components/Delivery_convenient";
 
 function Cartfilloutform() {
+  const [state, setState] = useState(false);
+  const [state2, setState2] = useState(false);
+  const [state3, setState3] = useState(false);
   const { cart } = useCart();
   return (
     <>
@@ -69,7 +70,25 @@ function Cartfilloutform() {
             <select
               className="stan_select_group"
               onChange={(e) => {
-                if (e.target.value === 1) {
+                if (e.target.value === "0") {
+                  setState(false);
+                  setState2(false);
+                  setState3(false);
+                }
+                if (e.target.value === "1") {
+                  setState(true);
+                  setState2(false);
+                  setState2(false);
+                }
+                if (e.target.value === "2") {
+                  setState2(true);
+                  setState3(false);
+                  setState(false);
+                }
+                if (e.target.value === "3") {
+                  setState3(true);
+                  setState2(false);
+                  setState(false);
                 }
               }}
             >
@@ -85,19 +104,172 @@ function Cartfilloutform() {
           </div>
 
           <div className="stan_fof_flex_left">
-            <select className="stan_select_group">
+            <select
+              className="stan_select_group"
+              onChange={(e) => {
+                if (e.target.value === 1) {
+                }
+              }}
+            >
               <option value="0">請選擇</option>
               <option value="1">信用卡</option>
               <option value="2">貨到付款</option>
             </select>
           </div>
 
-          <div className="stan_fof_flex">
+          <div
+            className={state || state2 || state3 ? "stan_fof_flex" : "stan_pagedisplay"}
+          >
             <div className="stan_h3">收件人資料</div>
           </div>
 
-          <Deliveryhome />
-          {/* <Deliveryconvenient /> */}
+          
+          <div className="stan_fof_flex_left ">
+            <div>
+              <form className="stan_fof_form">
+                <table>
+                  <tbody>
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="text" className="stan_fof_label">
+                          姓名<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr className="stan_input_tr">
+                      <td className="stan_input_td">
+                        <input
+                          type="text"
+                          className="stan_fof_input"
+                          required
+                        />
+                      </td>
+                    </tr>
+
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="phone" className="stan_fof_label">
+                          手機<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type="tel"
+                          id="phone"
+                          name="phone"
+                          className="stan_fof_input"
+                          pattern="[0-9]"
+                          required
+                        />
+                      </td>
+                    </tr>
+
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="email" className="stan_fof_label">
+                          電子信箱<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="stan_fof_input"
+                        />
+                      </td>
+                    </tr>
+
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="text" className="stan_fof_label">
+                          運送地址<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type="text"
+                          className="stan_fof_input"
+                          required
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form>
+            </div>
+          </div>
+
+          <div className="stan_fof_flex_left stan_convenient">
+            <div>
+              <form className="stan_fof_form">
+                <table>
+                  <tbody>
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="text" className="stan_fof_label">
+                          姓名<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr className="stan_input_tr">
+                      <td className="stan_input_td">
+                        <input
+                          type="text"
+                          className="stan_fof_input"
+                          required
+                        />
+                      </td>
+                    </tr>
+
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="phone" className="stan_fof_label">
+                          手機<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type="tel"
+                          id="phone"
+                          name="phone"
+                          className="stan_fof_input"
+                          pattern="[0-9]"
+                          required
+                        />
+                      </td>
+                    </tr>
+
+                    <tr className="stan_fof_tr">
+                      <td>
+                        <label htmlFor="email" className="stan_fof_label">
+                          電子信箱<span className="stan_fof_redword">*</span>
+                        </label>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="stan_fof_input"
+                        />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </form>
+            </div>
+          </div>
 
           <div className="stan_fof_btn">
             <div className="stan_fof_btn_back">上一頁</div>
--- src/pages/carts/components/Delivery_convenient.js
+++ src/pages/carts/components/Delivery_convenient.js
@@ -3,66 +3,7 @@ import React from "react";
 function Deliveryconvenient() {
   return (
     <>
-      <div className="stan_fof_flex_left">
-        <div>
-          <form className="stan_fof_form">
-            <table>
-              <tbody>
-                <tr className="stan_fof_tr">
-                  <td>
-                    <label htmlFor="text" className="stan_fof_label">
-                      姓名<span className="stan_fof_redword">*</span>
-                    </label>
-                  </td>
-                </tr>
-                <tr className="stan_input_tr">
-                  <td className="stan_input_td">
-                    <input type="text" className="stan_fof_input" required />
-                  </td>
-                </tr>
 
-                <tr className="stan_fof_tr">
-                  <td>
-                    <label htmlFor="phone" className="stan_fof_label">
-                      手機<span className="stan_fof_redword">*</span>
-                    </label>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <input
-                      type="tel"
-                      id="phone"
-                      name="phone"
-                      className="stan_fof_input"
-                      pattern="[0-9]"
-                      required
-                    />
-                  </td>
-                </tr>
-
-                <tr className="stan_fof_tr">
-                  <td>
-                    <label htmlFor="email" className="stan_fof_label">
-                      電子信箱<span className="stan_fof_redword">*</span>
-                    </label>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="stan_fof_input"
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
-        </div>
-      </div>
     </>
   );
 }
