a8c5976584b3de7728e96194d22fa2df3eceab97
TingYun <tingyun02@hotmail.com>
2022-03-30 01:02:14 +0800

    modal

--- src/pages/members/components/Signup.js
+++ src/pages/members/components/Signup.js
@@ -1,5 +1,5 @@
 
-import React,{useEffect, useState} from "react";
+import React,{useState} from "react";
 import Config from "../Config";
 import MemberModal from './MemberModal'
 import {Form} from 'react-bootstrap'
