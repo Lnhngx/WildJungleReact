408711b2b302c6b353fac338586b4f284ab5fd31
Lnhngx <a8817388064@gmail.com>
2022-03-30 23:53:42 +0800

    local

--- src/pages/products/components/productItem.js
+++ src/pages/products/components/productItem.js
@@ -15,33 +15,39 @@ function ProductItem(props) {
     ProductsPrice,
     ProductsDate,
   } = props.products;
+  const [heart, setHeart] = useState([{ ProductSid: "" }]);
+
   let MainPic = `img/product/${ProductsMainPic}`;
 
-  const click = function(){
-    //console.log(props.products)
-  }
+  const click =async function () {
+   await setHeart([{ProductSid:`${ProductSid}`}])
+   await  console.log(heart)
+  };
+
 
   return (
     <>
       <div className="productCard">
-        <Link to={`/productsdetail?id=${props.products.ProductSid}`} onClick={click}>
-          <div className="cardImg" >
+        <Link
+          to={`/productsdetail?id=${props.products.ProductSid}`}
+          
+        >
+          <div className="cardImg">
             <img className="" src={MainPic} alt="" />
           </div>
         </Link>
         <div className="cardName">
-          <Link className="cardTitle" to="">
+          <Link
+            className="cardTitle"
+            to={`/productsdetail?id=${props.products.ProductSid}`}
+          >
             <p>{ProductsName}</p>
             <span>{ProductsVendor}</span>
             <span>${ProductsPrice}</span>
           </Link>
           <div className="cardIcon">
-            <Link to="">
-              <i className="fas fa-heart"></i>
-            </Link>
-            <Link to="">
-              <i className="fas fa-shopping-cart"></i>
-            </Link>
+            <i className="fas fa-heart" onClick={click}></i>
+            <i className="fas fa-shopping-cart"></i>
           </div>
         </div>
       </div>
