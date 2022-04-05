2eb1aa72741ed1f416c9f228c7edc4105ae0b49f
TingYun <tingyun02@hotmail.com>
2022-04-05 16:09:39 +0800

    card

--- src/pages/members/components/CreditcardAdd.js
+++ src/pages/members/components/CreditcardAdd.js
@@ -84,7 +84,7 @@ export default class CreditcardAdd extends React.Component {
                 let newAr=Object.keys(newCredit).map((v)=> newCredit[v] )
                 console.log(newAr)
                 // this.props.setLocalCredit(newAr);
-                newAr.push({credit_sid: obj.info, credit_num: formData.number, credit_name: formData.name, credit_date: formData.expiry, credit_code:formData.cvc});
+                newAr.push({credit_sid: obj.info, credit_num: formData.number, credit_name: formData.name, credit_date: formData.expiry, credit_code:parseInt(formData.cvc)});
                 console.log(newAr)
                 this.props.setCreditData(newAr);
 
