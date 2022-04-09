f6002eebf00dae7441ca9699a38bf2324ed2fae9
Stan Lin <love4210kimo@gmail.com>
2022-04-09 16:56:34 +0800

    登出時寫進資料庫，清除購物車

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,20 +6,19 @@ import { useSecondCart } from "../pages/carts/utils/useSecondCart";
 import { useThirdCart } from "../pages/carts/utils/useThirdCart";
 import { useFourthCart } from "../pages/carts/utils/useFourthCart";
 import "../components/all.scss";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar(props) {
   const firstcart = useCart();
   const secondcart = useSecondCart();
   const thirdcart = useThirdCart();
   const fourthcart = useFourthCart();
-  const allcartItems = (
+  const allcartItems =
     firstcart.cart.totalItems +
     secondcart.cart.totalItems +
     thirdcart.cart.totalItems +
-    fourthcart.cart.totalItems)
-
+    fourthcart.cart.totalItems;
 
   const [nav, setNav] = useState(0);
   const { auth, setAuth, localState, setLocalState, setComeUrl } = props;
@@ -59,9 +58,6 @@ function Navbar(props) {
     };
   }, [nav]);
 
-
-
-
   window.addEventListener("load", () => {
     if (
       window.localStorage.getItem("like") === null ||
@@ -70,7 +66,6 @@ function Navbar(props) {
       localStorage.setItem("like", JSON.stringify([]));
     }
   });
-
 
   return (
     <>
@@ -89,47 +84,106 @@ function Navbar(props) {
           <div className="navbarIcon">
             {auth || localState.token ? (
               <>
-                <FontAwesomeIcon icon={faHouse} className="tysu_logInOut" onClick={() => {
-                  if (showOut) {
-                    setShowOut(false)
-                  } else {
-                    setShowOut(true)
-                  }
-                }} />
+                <FontAwesomeIcon
+                  icon={faHouse}
+                  className="tysu_logInOut"
+                  onClick={() => {
+                    if (showOut) {
+                      setShowOut(false);
+                    } else {
+                      setShowOut(true);
+                    }
+                  }}
+                />
               </>
             ) : (
               <>
-                <i className="fas fa-user-friends tysu_logInOut"
+                <i
+                  className="fas fa-user-friends tysu_logInOut"
                   onClick={() => {
                     // console.log(location.pathname)
-                    setComeUrl(location.pathname)
+                    setComeUrl(location.pathname);
                     history.push("/members");
                   }}
                 ></i>
               </>
             )}
-            {showOut && <div className="tysu_logInfo">
-              <ul>
-                <li onClick={() => {
-                  history.push('/members/modify-member-info')
-                  setShowOut(false)
-                }}>會員專區</li>
-                <li onClick={() => {
-                  // localStorage.setItem("like",JSON.stringify([]));
-                  localStorage.removeItem("admin_account");
-                  localStorage.removeItem("admin_token");
-                  setAuth(false);
-                  setLocalState({ token: false });
-                  setShowOut(false);
-                }}>登出</li>
-              </ul>
-            </div>}
+            {showOut && (
+              <div className="tysu_logInfo">
+                <ul>
+                  <li
+                    onClick={() => {
+                      history.push("/members/modify-member-info");
+                      setShowOut(false);
+                    }}
+                  >
+                    會員專區
+                  </li>
+                  <li
+                    onClick={() => {
+                      const m_sid = JSON.parse(
+                        localStorage.getItem("admin_account")
+                      ).m_sid;
+
+                      const cart_temp1 = JSON.parse(
+                        localStorage.getItem("cart")
+                      );
+                      console.log(cart_temp1);
+                      const cart_temp2 = JSON.parse(
+                        localStorage.getItem("secondCart")
+                      );
+                      const cart_temp3 = JSON.parse(
+                        localStorage.getItem("thirdCart")
+                      );
+                      const cart_temp4 = JSON.parse(
+                        localStorage.getItem("fourthCart")
+                      );
+
+                      fetch("http://localhost:4000/carts/inserttodb", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                          m_sid: m_sid,
+                          cart_temp1: cart_temp1,
+                          cart_temp2: cart_temp2,
+                          cart_temp3: cart_temp3,
+                          cart_temp4: cart_temp4,
+                        }),
+                      })
+                        .then((r) => r.json())
+                        .then((obj) => {});
+
+                      localStorage.setItem("like", JSON.stringify([]));
+                      localStorage.removeItem("admin_account");
+                      localStorage.removeItem("admin_token");
+                      firstcart.clearCart();
+                      secondcart.clearCart();
+                      thirdcart.clearCart();
+                      fourthcart.clearCart();
+                      setAuth(false);
+                      setLocalState({ token: false });
+                      setShowOut(false);
+                    }}
+                  >
+                    登出
+                  </li>
+                </ul>
+              </div>
+            )}
             <a href="/members/modify-member-info">
               <i className="fas fa-heart fixedPosition"></i>
             </a>
             <a href="/carts">
               <i className="fas fa-shopping-cart stan_cartIcon">
-                <div className={allcartItems >= 10 ? "stan_cartIcon_count_over10" : "stan_cartIcon_count"}>{allcartItems}</div>
+                <div
+                  className={
+                    allcartItems >= 10
+                      ? "stan_cartIcon_count_over10"
+                      : "stan_cartIcon_count"
+                  }
+                >
+                  {allcartItems}
+                </div>
               </i>
             </a>
           </div>
@@ -170,9 +224,17 @@ function Navbar(props) {
           <Link to="/game">
             <div>Animal Games</div>
           </Link>
-          {auth || localState.token ? <Link to="/members/modify-member-info"> <div>Members</div>
-          </Link> : <Link to="/members"> <div>Members</div>
-          </Link>}
+          {auth || localState.token ? (
+            <Link to="/members/modify-member-info">
+              {" "}
+              <div>Members</div>
+            </Link>
+          ) : (
+            <Link to="/members">
+              {" "}
+              <div>Members</div>
+            </Link>
+          )}
           <Link to="/carts">
             <div>Shopping Cart</div>
           </Link>
